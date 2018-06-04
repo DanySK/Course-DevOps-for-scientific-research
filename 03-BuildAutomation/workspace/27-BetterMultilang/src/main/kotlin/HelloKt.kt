517c2c3f01c64154ef6896d6e5b5fa8a82c271e0
Danilo Pianini <danilo.pianini@gmail.com>
2018-06-05 01:42:13 +0200

    Improve Build automation examples

---
+++ 03-BuildAutomation/workspace/27-BetterMultilang/src/main/kotlin/HelloKt.kt
@@ -0,0 +1,4 @@
+fun main(args : Array<String>) { 
+    HelloWorld.main()
+    println("Hello, world from Kotlin!") 
+}
---
+++ 03-BuildAutomation/workspace/27-BetterMultilang/src/main/scala/HelloScala.scala
@@ -0,0 +1,4 @@
+object HelloScala extends App  {
+    HelloWorld.main()
+    println("Hello Scala")
+}
