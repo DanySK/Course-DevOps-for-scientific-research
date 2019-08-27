5b85fdf7ae3426033cf9984cf20bccda9d746058
Danilo Pianini <danilo.pianini@unibo.it>
2019-08-27 18:38:26 +0200

    add debug prints

--- compile.sh
+++ compile.sh
@@ -3,6 +3,9 @@ set -e
 for pack in 0*
 do
     cd $pack
+    echo current position:
+    pwd
+    echo current content:
     ls -ahl
     pdflatex -interaction=nonstopmode -halt-on-error -shell-escape "${pack}.tex"
     cd ..
