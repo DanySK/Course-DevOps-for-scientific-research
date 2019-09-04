790dae673dfa27ddbddd92984c9b6247a626de1b
Danilo Pianini <danilo.pianini@gmail.com>
2019-09-04 20:48:05 +0200

    Fix up to 20

---
+++ 03-BuildAutomation/workspace/19-Coverage/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    java
+    jacoco
+}
+
+repositories { mavenCentral() }
+
+dependencies {
+    testImplementation("junit:junit:4.12")
+}
---
+++ 03-BuildAutomation/workspace/20-Checkstyle/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    java
+    checkstyle
+    jacoco
+}
+
+repositories { mavenCentral() }
+
+dependencies {
+    testImplementation("junit:junit:4.12")
+}
