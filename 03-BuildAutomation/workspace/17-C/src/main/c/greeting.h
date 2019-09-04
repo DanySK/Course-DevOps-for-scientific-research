cdab280ffb476b1b1011615ba6e5495b219bd717
Danilo Pianini <danilo.pianini@gmail.com>
2019-09-04 18:28:14 +0200

    Refresh C/C++

--- 03-BuildAutomation/workspace/17-C/src/main/c/greeting.h
+++
@@ -1,6 +0,0 @@
-#ifndef EXAMPLE_GREETING_H
-#define EXAMPLE_GREETING_H
-
-#define GREETING_STRING "Hello, C native executable!"
-
-#endif
--- 03-BuildAutomation/workspace/17-C/src/main/c/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>  
-#include "greeting.h" 
-
-int main(void) {
-    printf("%s\n", GREETING_STRING);
-    return 0;
-}
